35c46156c1066dda5af519541a22afc4b8947cea
nilufarmn <56490211+nilufarmn@users.noreply.github.com>
2019-12-13 06:34:23 -0800

    completed unit test with jest
    write first test using tobe,expect function from jest

--- util.test.js
+++ util.test.js
@@ -1,2 +1,6 @@
-const {generateText}=require('/util');//how to impot a function in our unit test
-test('git')
+const {generateText}=require('./util');//how to impot a function in our unit test
+test('should output name and age', ( ) => {
+const text =generateText('niloofar',22);
+expect(text).toBe('niloofar (22 years old)');
+
+});
